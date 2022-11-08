651dfca465d528938f91e612a6ab278f4a952b4e
Sebastian Lamu <sebastian.lamu@gmail.com>
2022-11-08 08:18:20 +0100

    Update ostbyggogtak.js

--- ostbyggogtak.js
+++ ostbyggogtak.js
@@ -1,6 +1,5 @@
-
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal2");
 
 // Get the button that opens the modal
 
@@ -8,8 +7,16 @@ var btn1 = document.getElementById("modalBtn1");
 var btn2 = document.getElementById("modalBtn2");
 var btn3 = document.getElementById("modalBtn3");
 var btn4 = document.getElementById("modalBtn4");
+var btn5 = document.getElementById("modalBtn5");
+var btn6 = document.getElementById("modalBtn6");
+var btn7 = document.getElementById("modalBtn7");
+var btn8 = document.getElementById("modalBtn8");
+var btn9 = document.getElementById("modalBtn9");
+var btn10 = document.getElementById("modalBtn10");
+var btn11 = document.getElementById("modalBtn11");
 
-var modalImg = document.getElementById("modalImg");
+
+var modalDiv = document.getElementById("modalDiv");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -17,9 +24,60 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn1.onclick = function() {
   modal.style.display = "block";
-  modalImg.src="https://image.vartoslo.no/411994.jpg?imageId=411994&x=0&y=0&cropw=100&croph=100&width=751&height=657";
+  modalDiv.innerHTML = this.innerHTML;
 };
 
+btn2.onclick = function() {
+  modal.style.display = "block";
+  modalDiv.innerHTML = this.innerHTML;
+};
+
+btn3.onclick = function() {
+  modal.style.display = "block";
+  modalDiv.innerHTML = this.innerHTML;
+};
+
+btn4.onclick = function() {
+  modal.style.display = "block";
+  modalDiv.innerHTML = this.innerHTML;
+};
+
+btn5.onclick = function() {
+  modal.style.display = "block";
+  modalDiv.innerHTML = this.innerHTML;
+};
+
+btn6.onclick = function() {
+  modal.style.display = "block";
+  modalDiv.innerHTML = this.innerHTML;
+};
+
+btn7.onclick = function() {
+  modal.style.display = "block";
+  modalDiv.innerHTML = this.innerHTML;
+};
+
+btn8.onclick = function() {
+  modal.style.display = "block";
+  modalDiv.innerHTML = this.innerHTML;
+};
+
+btn9.onclick = function() {
+  modal.style.display = "block";
+  modalDiv.innerHTML = this.innerHTML;
+};
+
+btn10.onclick = function() {
+  modal.style.display = "block";
+  modalDiv.innerHTML = this.innerHTML;
+};
+
+btn11.onclick = function() {
+  modal.style.display = "block";
+  modalDiv.innerHTML = this.innerHTML;
+};
+
+/*
 // When the user clicks on the button, open the modal
 btn2.onclick = function() {
   modal.style.display = "block";
@@ -37,6 +95,7 @@ btn4.onclick = function() {
   modal.style.display = "block";
   modalImg.src="https://image.vartoslo.no/412247.jpg?imageId=412247&x=0&y=0&cropw=100&croph=100&width=751&height=657";
 };
+*/
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -61,11 +120,11 @@ function showSlides() {
     slides[i].style.display = "none";  
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (slideIndex > slides.length) {slideIndex = 1};
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" activeDot", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " activeDot";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
