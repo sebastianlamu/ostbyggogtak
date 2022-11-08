15e3201a8c6113f92d1ac25f30336b352ea46d63
Sebastian Lamu <sebastian.lamu@gmail.com>
2022-11-08 08:29:05 +0100

    Update ostbyggogtak.js

--- ostbyggogtak.js
+++ ostbyggogtak.js
@@ -3,17 +3,17 @@ var modal = document.getElementById("myModal2");
 
 // Get the button that opens the modal
 
-var btn1 = document.getElementById("modalBtn1");
-var btn2 = document.getElementById("modalBtn2");
-var btn3 = document.getElementById("modalBtn3");
-var btn4 = document.getElementById("modalBtn4");
-var btn5 = document.getElementById("modalBtn5");
-var btn6 = document.getElementById("modalBtn6");
-var btn7 = document.getElementById("modalBtn7");
-var btn8 = document.getElementById("modalBtn8");
-var btn9 = document.getElementById("modalBtn9");
-var btn10 = document.getElementById("modalBtn10");
-var btn11 = document.getElementById("modalBtn11");
+var bttn1 = document.getElementById("modalBttn1");
+var bttn2 = document.getElementById("modalBttn2");
+var bttn3 = document.getElementById("modalBttn3");
+var bttn4 = document.getElementById("modalBttn4");
+var bttn5 = document.getElementById("modalBttn5");
+var bttn6 = document.getElementById("modalBttn6");
+var bttn7 = document.getElementById("modalBttn7");
+var bttn8 = document.getElementById("modalBttn8");
+var bttn9 = document.getElementById("modalBttn9");
+var bttn10 = document.getElementById("modalBttn10");
+var bttn11 = document.getElementById("modalBttn11");
 
 
 var modalDiv = document.getElementById("modalDiv");
@@ -22,80 +22,60 @@ var modalDiv = document.getElementById("modalDiv");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn1.onclick = function() {
+bttn1.onclick = function() {
   modal.style.display = "block";
   modalDiv.innerHTML = this.innerHTML;
 };
 
-btn2.onclick = function() {
+bttn2.onclick = function() {
   modal.style.display = "block";
   modalDiv.innerHTML = this.innerHTML;
 };
 
-btn3.onclick = function() {
+bttn3.onclick = function() {
   modal.style.display = "block";
   modalDiv.innerHTML = this.innerHTML;
 };
 
-btn4.onclick = function() {
+bttn4.onclick = function() {
   modal.style.display = "block";
   modalDiv.innerHTML = this.innerHTML;
 };
 
-btn5.onclick = function() {
+bttn5.onclick = function() {
   modal.style.display = "block";
   modalDiv.innerHTML = this.innerHTML;
 };
 
-btn6.onclick = function() {
+bttn6.onclick = function() {
   modal.style.display = "block";
   modalDiv.innerHTML = this.innerHTML;
 };
 
-btn7.onclick = function() {
+bttn7.onclick = function() {
   modal.style.display = "block";
   modalDiv.innerHTML = this.innerHTML;
 };
 
-btn8.onclick = function() {
+bttn8.onclick = function() {
   modal.style.display = "block";
   modalDiv.innerHTML = this.innerHTML;
 };
 
-btn9.onclick = function() {
+bttn9.onclick = function() {
   modal.style.display = "block";
   modalDiv.innerHTML = this.innerHTML;
 };
 
-btn10.onclick = function() {
+bttn10.onclick = function() {
   modal.style.display = "block";
   modalDiv.innerHTML = this.innerHTML;
 };
 
-btn11.onclick = function() {
+bttn11.onclick = function() {
   modal.style.display = "block";
   modalDiv.innerHTML = this.innerHTML;
 };
-
-/*
-// When the user clicks on the button, open the modal
-btn2.onclick = function() {
-  modal.style.display = "block";
-  modalImg.src="https://image.vartoslo.no/411999.jpg?imageId=411999&x=0&y=0&cropw=100&croph=100&width=751&height=657";
-};
-
-// When the user clicks on the button, open the modal
-btn3.onclick = function() {
-  modal.style.display = "block";
-  modalImg.src="https://image.vartoslo.no/412246.jpg?imageId=412246&x=0&y=0&cropw=100&croph=100&width=751&height=657";
-};
-
-// When the user clicks on the button, open the modal
-btn4.onclick = function() {
-  modal.style.display = "block";
-  modalImg.src="https://image.vartoslo.no/412247.jpg?imageId=412247&x=0&y=0&cropw=100&croph=100&width=751&height=657";
-};
-*/
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -122,9 +102,9 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1};
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" activeDot", "");
+    dots[i].className = dots[i].className.replace(" aktiv", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " activeDot";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+  dots[slideIndex-1].className += " aktiv";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
